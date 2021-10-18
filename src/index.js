f5faa1e0c9ed7ecb5692d2c7114ce214b525a1ed
jessica-au <AU.JESSICA.M@GMAIL.COM>
2021-10-18 14:53:34 -0700

    Move port to .env

--- src/index.js
+++ src/index.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
   res.send('hello world');
 });
 
-app.listen(3000, () => 
-console.log('Example app listening on port 3000')
+app.listen(process.env.PORT, () => 
+console.log(`Example app listening on ${process.env.PORT}`)
 );
